f77926c604cb96a96e9234743650a39812055ab8
phatcharalaw <46377227+phatcharalaw@users.noreply.github.com>
2019-03-28 17:21:07 +0700

    Add files via upload
    first

---
+++ demo-first-app/src/main/java/com/pch/allianz/demofirstapp/DemoFirstAppApplication.java
@@ -0,0 +1,18 @@
+package com.pch.allianz.demofirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstAppApplication.class, args);
+	}
+
+	private void himethod(){
+
+	}
+
+}
+
